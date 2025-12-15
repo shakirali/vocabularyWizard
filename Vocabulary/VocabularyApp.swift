6787cce66aedd6a9ecfc23958cace08d2e9d2701
Shakir Ali <shakirmithani@gmail.com>
2025-12-15 11:43:18 +0000

    Initial Commit

---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Shakir Ali on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocabularyTests/VocabularyTests.swift
@@ -0,0 +1,16 @@
+//
+//  VocabularyTests.swift
+//  VocabularyTests
+//
+//  Created by Shakir Ali on 15/12/2025.
+//
+
+import Testing
+
+struct VocabularyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
